38c13965b19e2c7b2bf74179a4baa662a6388025
Waterme10n <huer2783@naver.com>
2014-10-14 13:07:07 +0900

    1

--- android/gen/com/comma/weathercloset/R.java
+++ android/gen/com/comma/weathercloset/R.java
@@ -1097,15 +1097,19 @@ containing a value of this type.
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
+        public static final int homeBtn=0x7f050042;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int myPageBtn=0x7f050041;
         public static final int never=0x7f050009;
+        public static final int newPostBtn=0x7f050040;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int noticeBtn=0x7f050043;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1119,13 +1123,18 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
+        public static final int settingBtn=0x7f050044;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spinner=0x7f05003c;
         public static final int split_action_bar=0x7f05001e;
+        public static final int statisticsTab=0x7f05003f;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int tabhost=0x7f05003d;
+        public static final int timelineTab=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1228,7 +1237,13 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
-        public static final int homeBtn=0x7f0a000f;
+        public static final int homeBtn=0x7f0a0013;
+        public static final int myPageBtn=0x7f0a0012;
+        public static final int newPostBtn=0x7f0a0011;
+        public static final int noticeBtn=0x7f0a0014;
+        public static final int settingBtn=0x7f0a0015;
+        public static final int statisticsTab=0x7f0a0010;
+        public static final int timelineTab=0x7f0a000f;
     }
     public static final class style {
         /** 
--- android/src/com/comma/weathercloset/MainActivity.java
+++ android/src/com/comma/weathercloset/MainActivity.java
@@ -2,14 +2,81 @@ package com.comma.weathercloset;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
+	String[] items = { "서울", "대전", "대구", "부산" };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		setUpSpinner();
+		setUpTabs();
+	}
+
+	private void setUpSpinner() {
+		Spinner spin = (Spinner) findViewById(R.id.spinner);
+		spin.setOnItemSelectedListener(this);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				this, android.R.layout.simple_spinner_item, items);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		spin.setAdapter(adapter);
+	}
+	
+	private void setUpTabs() {
+		TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+		tabHost.setup();
+		
+		TabSpec spec1 = tabHost.newTabSpec("timelineTab").setContent(R.id.timelineTab).
+				setIndicator(getString(R.string.timelineTab));
+		tabHost.addTab(spec1);
+		
+		TabSpec spec2 = tabHost.newTabSpec("statisticsTab").setContent(R.id.statisticsTab).
+				setIndicator(getString(R.string.statisticsTab));
+		tabHost.addTab(spec1);
+		
+		tabHost.getTabWidget().getChildAt(0).getLayoutParams().height = 80;
+		tabHost.getTabWidget().getChildAt(1).getLayoutParams().height = 80;
+		
+		RelativeLayout.LayoutParams tvParams = new RelativeLayout.LayoutParams
+				(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		
+		RelativeLayout rl1 = (RelativeLayout) tabHost.getTabWidget().getChildAt(0);
+		rl1.setGravity(Gravity.CENTER_VERTICAL);
+		TextView tv1 = (TextView) rl1.getChildAt(0);
+		tv1.setLayoutParams(tvParams);
+		tv1.setTextAppearance(this, android.R.style.TextAppearance_Medium);
+		tv1.setPadding(10, 0, 10, 0);
+		tv1.setGravity(Gravity.CENTER);
+		
+		RelativeLayout rl2 = (RelativeLayout) tabHost.getTabWidget().getChildAt(1);
+		rl2.setGravity(Gravity.CENTER_VERTICAL);
+		TextView tv2 = (TextView) rl2.getChildAt(1);
+		tv2.setLayoutParams(tvParams);
+		tv2.setTextAppearance(this, android.R.style.TextAppearance_Medium);
+		tv2.setPadding(10, 0, 10, 0);
+		tv2.setGravity(Gravity.CENTER);
+	}
+	
+	public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+	}
+
+	public void onNothingSelected(AdapterView<?> parent) {
+
+	}
+	
 }
