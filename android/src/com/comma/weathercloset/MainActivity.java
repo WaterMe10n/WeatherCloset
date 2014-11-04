d648e8b7bf0975bfd982d64db3c418bc390ae690
Waterme10n <huer2783@naver.com>
2014-11-04 13:10:02 +0900

    fragment중

--- android/src/com/comma/weathercloset/MainActivity.java
+++ android/src/com/comma/weathercloset/MainActivity.java
@@ -1,16 +1,24 @@
 package com.comma.weathercloset;
 
-import android.app.Activity;
+import android.app.*;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TabHost;
 
-public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+public class MainActivity extends FragmentActivity implements AdapterView.OnItemSelectedListener {
+	
+	int mCurrentFragmentIndex;
+	public final static int FRAGMENT_ONE = 0;
+	public final static int FRAGMENT_TWO = 1;
+	
 	String[] items = { "서울", "대전", "대구", "부산" };
 
 	@Override
@@ -18,6 +26,13 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Button timelineFragment = (Button) findViewById(R.id.timelineFragment);
+		timelineFragment.setOnClickListener((OnClickListener) this);
+		Button statisticsFragment = (Button) findViewById(R.id.statisticsFragment);
+		statisticsFragment.setOnClickListener((OnClickListener) this);
+		
+		mCurrentFragmentIndex = FRAGMENT_ONE;
+		fragmentReplace(mCurrentFragmentIndex);
 		
 		ImageView myPageBtn = (ImageView)findViewById(R.id.myPageBtn);
 		myPageBtn.setOnClickListener(myPageBtnClick);
@@ -25,6 +40,60 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
 		//setUpTabs();
 		setUpSpinner();
 		
+	}
+	
+	public void fragmentReplace(int reqNewFragmentIndex) {
+
+		Fragment newFragment = null;
+
+		Log.d(TAG, "fragmentReplace " + reqNewFragmentIndex);
+
+		newFragment = getFragment(reqNewFragmentIndex);
+
+		// replace fragment
+		final FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+
+		transaction.replace(R.id.main_fragment, newFragment);
+
+		// Commit the transaction
+		transaction.commit();
+
+	}
+
+	private Fragment getFragment(int idx) {
+		Fragment newFragment = null;
+
+		switch (idx) {
+		case FRAGMENT_ONE:
+			newFragment = new OneFragment();
+			break;
+		case FRAGMENT_TWO:
+			newFragment = new TwoFragment();
+			break;
+			
+		default:
+			Log.d(TAG, "Unhandle case");
+			break;
+		}
+
+		return newFragment;
+	}
+
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+
+		case R.id.timelineFragment:
+			mCurrentFragmentIndex = FRAGMENT_ONE;
+			fragmentReplace(mCurrentFragmentIndex);
+			break;
+		case R.id.statisticsFragment:
+			mCurrentFragmentIndex = FRAGMENT_TWO;
+			fragmentReplace(mCurrentFragmentIndex);
+			break;
+		}
+
 	}
 	
 	ImageView.OnClickListener myPageBtnClick = new ImageView.OnClickListener()
